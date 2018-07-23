daafb9a6c6734497fe57605aa0373cfcc4a100a1
anvitha-b <anvithaboosani@gmail.com>
2018-07-23 20:59:56 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,26 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plants legs far apart, bend knees slightly and keep posture loose"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip_step backward"
+  
+def skip_step (left)
+  puts "Step to the left"
+end
+
+def skip_step (right)
+  puts "Step to the left"
+end
+
+upper body
+def bob_the_reins
+  puts "Hold your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down"
+end
+
